a09a61d9870e774e1795b6e326a2f9bf1bf86369
IDN444 <dananaumova67@gmail.com>
2025-12-02 19:36:22 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    pass
+# пункт а
+height = float(input())
+if height < 0 or height > 300:
+     raise ValueError ("Некорректный рост")
+else:
+    print ("Рост введен корректно")
+
+# пункт б
+weight = float(input())
+if weight < 0 or weight > 500:
+     raise ValueError ("Некорректный вес")
+else:
+    print ("Вес введен корректно")
+
+# пункт с
+temp = float(input())
+if temp < -273.15:
+    raise ValueError ("Некорректная температура")
+else:
+    print ("Температура введена корректно")
