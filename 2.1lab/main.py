e0969a25dcbdb1e20ec677be8d4ce15ffd4dbf4d
IDN444 <dananaumova67@gmail.com>
2025-12-06 22:08:57 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    pass
+#пункт а
+def sum_values(x):
+    total = sum(x.values())
+    return total
+dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+print(sum_values(dict))
+
+#пункт б
+def list_dict(x):
+    dict = {}
+    for item in x:
+        dict[item] = {}
+    return dict
+list = ['a', 'b', 'c']
+print(list_dict(list))
+
+#пункт в
+def values_equal(x):
+    values = list(x.values())
+    return all(y == values[0] for y in values)
+dict = {'a': 1, 'b': 1, 'c': 1}
+print(values_equal(dict))
