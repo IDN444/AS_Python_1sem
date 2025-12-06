e2f3925ed94c3bcabccc638bf61f787029b87168
IDN444 <dananaumova67@gmail.com>
2025-12-06 22:11:52 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+def Quater(x,y):
+    if x > 0 and y > 0:
+        return 1
+    if x < 0 and y > 0:
+        return 2
+    if x < 0 and y < 0:
+        return 3
+    if x > 0 and y < 0:
+        return 4
