2266cf898328fd08f87b7a0ce31e464a8cafeb4e
IDN444 <dananaumova67@gmail.com>
2025-12-15 20:03:28 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,4 +1,4 @@
-#gункт а
+#пункт а
 def filter_numbers(numbers, condition = None):
     if condition is None:
         return numbers
@@ -6,4 +6,23 @@ def filter_numbers(numbers, condition = None):
     for number in numbers:
         if condition(number):
             result.append(number)
+    return result
+
+#пункт б
+def transform_numbers(numbers, transform_function = None):
+    if transform_function is None:
+        return numbers
+    result = []
+    for number in numbers:
+        transformed_number = transform_function(number)
+        result.append(transformed_number)
+    return result
+
+#пункт в
+def cube_numbers(lists):
+    result = []
+    for numbers_list in lists:
+        for number in numbers_list:
+            cube = number ** 3
+            result.append(cube)
     return result
