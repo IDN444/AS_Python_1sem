99c6716999b798b91030d65d8f7bbe231d04d8c1
IDN444 <dananaumova67@gmail.com>
2025-12-15 19:41:03 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+#gункт а
+def filter_numbers(numbers, condition = None):
+    if condition is None:
+        return numbers
+    result = []
+    for number in numbers:
+        if condition(number):
+            result.append(number)
+    return result
