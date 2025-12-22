c8755294da6cca93bdf9cf14821b8c03568cc05f
IDN444 <dananaumova67@gmail.com>
2025-12-22 17:27:26 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -1,14 +1,14 @@
 import math
-def abs(a, b):
+def num_abs(a, b):
     return math.sqrt(a * a + b * b)
 
 
-def mult(a1, b1, a2, b2):
+def num_mult(a1, b1, a2, b2):
     x = a1 * a2 - b1 * b2
     y = a1 * b2 + b1 * a2
     return x, y
 
-def div(a1, b1, a2, b2):
+def num_div(a1, b1, a2, b2):
     z = a2 * a2 + b2 * b2
     x = (a1 * a2 + b1 * b2) / z
     y = (b1 * a2 - a1 * b2) / z
