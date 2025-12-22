8dba79c2c9d33d6d2c41339c2a32db23b690a0bb
IDN444 <dananaumova67@gmail.com>
2025-12-22 17:26:52 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+with open ("text.txt", "r", encoding = "utf-8") as file:
+    paragraph = 0
+    in_paragraph = False
+
+for line in file:
+    if line.strip():
+        if not in_paragraph:
+            paragraph +=1
+            in_paragraph = True
+        else:
+            in_paragraph = False
+            
+print(paragraph)
