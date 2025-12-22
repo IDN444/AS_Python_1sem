6707bb6b575c3b2aa967c5df5779688796a1ad9e
IDN444 <dananaumova67@gmail.com>
2025-12-22 17:54:47 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+with open("text.txt", "r", encoding = "utf-8") as file:
+    print("Весенние даты")
+
+for line in file:
+    line = line.strip()
+    if line == "":
+        continue
+    
+    day, month, year = map(int, line.split())
+    if month == 3 or month == 4 or month == 5:
+        print(day, month, year)
